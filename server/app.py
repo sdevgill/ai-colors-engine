27b9ed010d4d117e4054922d71b292201cd58b43
Dev Gill <26435947+sdevgill@users.noreply.github.com>
2023-04-21 21:25:54 +0100

    Update server to gpt

--- server/app.py
+++ server/app.py
@@ -18,31 +18,39 @@ app = Flask(
 
 
 def get_colors(msg):
-    prompt = f"""
-    You are a color palette generating assistant that responds to text prompts for color palettes
-    Your should generate color palettes that fit the theme, mood, or instructions in the prompt.
-    The palettes should be between 2 and 8 colors.
-
-    Q: Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea
-    A: ["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]
-
-    Q: Convert the following verbal description of a color palette into a list of colors: sage, nature, earth
-    A: ["#EDF1D6", "#9DC08B", "#609966", "#40513B"]
-
-
-    Desired Format: a JSON array of hexadecimal color codes
-
-    Q: Convert the following verbal description of a color palette into a list of colors: {msg}
-    A:
-    """
-
-    response = openai.Completion.create(
-        prompt=prompt,
-        model="text-davinci-003",
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a color palette generating assistant that responds to text prompts for color palettes. You should generate color palettes that fit the theme, mood, or instructions in the prompt.The palettes should be between 2 and 8 colors.",
+        },
+        {
+            "role": "user",
+            "content": "Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea",
+        },
+        {
+            "role": "assistant",
+            "content": '["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]',
+        },
+        {
+            "role": "user",
+            "content": "Convert the following verbal description of a color palette into a list of colors: sage, nature, earth",
+        },
+        {
+            "role": "assistant",
+            "content": '["#EDF1D6", "#9DC08B", "#609966", "#40513B"]',
+        },
+        {
+            "role": "user",
+            "content": f"Convert the following verbal description of a color palette into a list of colors: {msg}",
+        },
+    ]
+    response = openai.ChatCompletion.create(
+        messages=messages,
+        model="gpt-3.5-turbo",  # use gpt-3.5-turbo if you don't have gpt-4 access
         max_tokens=200,
     )
 
-    colors = json.loads(response["choices"][0]["text"])
+    colors = json.loads(response["choices"][0]["message"]["content"])
     return colors
 
 
